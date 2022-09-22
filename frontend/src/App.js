ca98555919db301c0a966240f4f7906719f08b9f
Daniel <23vietduc@gmail.com>
2022-09-22 23:01:44 +0200

    Testing frontend CI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Testing merging PR.
+          Edit <code>src/App.js</code> and save to reload. Merged PR.
         </p>
         <a
           className="App-link"
