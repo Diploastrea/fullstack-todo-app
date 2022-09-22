b369b06fed56cd409182e6ceb6a044f3466bc762
Daniel <23vietduc@gmail.com>
2022-09-22 17:41:55 +0200

    Implemented sequelize

---
+++ backend/src/db/config/config.js
@@ -0,0 +1,26 @@
+const config = require('../../config');
+
+module.exports = {
+  development: {
+    username: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+    host: config.mysql.host,
+    dialect: 'mysql',
+    timezone: '+02:00',
+  },
+  test: {
+    username: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+    host: config.mysql.host,
+    dialect: 'mysql',
+  },
+  production: {
+    username: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+    host: config.mysql.host,
+    dialect: 'mysql',
+  },
+};
--- backend/src/services/registerService.js
+++ backend/src/services/registerService.js
@@ -1,8 +1,14 @@
-import { validateRegistration } from '../utils';
+export function validate(name, email, password) {
+  if (!name && !email && !password) throw new Error('emptyFields');
+  if (!name) throw new Error('emptyName');
+  if (!email) throw new Error('emptyEmail');
+  if (!password) throw new Error('emptyPassword');
+  if (password.length < 8) throw new Error('shortPassword');
+}
 
 export const registerService = {
   async registerUser(name, email, password) {
-    validateRegistration(name, email, password);
+    validate(name, email, password);
     return { status: 'success' };
   },
 };
