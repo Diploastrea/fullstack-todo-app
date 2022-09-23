9c6d654bd2c7e8f9f596a1c09aa1216ee15809e8
Daniel <23vietduc@gmail.com>
2022-09-23 12:02:24 +0200

    Added react-router-dom dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,8 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello world, testing firebase.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      Hello world!
+    </p>
   );
 }
 
