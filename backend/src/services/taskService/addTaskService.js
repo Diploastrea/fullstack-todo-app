bb302288bffa40305d9a99dbcfaae63a8b2ac3af
Daniel <23vietduc@gmail.com>
2022-09-26 22:08:47 +0200

    Finished addTaskService

--- backend/src/services/taskService/addTaskService.js
+++ backend/src/services/taskService/addTaskService.js
@@ -1,3 +1,17 @@
-export const addTaskService = {
+import { Task } from '../../models/Task';
 
+export async function add(description, priority, dueDate, userId) {
+  await Task.create({
+    description,
+    priority,
+    dueDate,
+    userId,
+  });
+}
+
+export const addTaskService = {
+  async addTask(description, priority, dueDate, userId) {
+    await add(description, priority, dueDate, userId);
+    return { status: 'ok' };
+  },
 };
