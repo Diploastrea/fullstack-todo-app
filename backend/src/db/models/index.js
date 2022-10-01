a8a36b1eb3aa5c1e5425670cfdc7f8adc129ec74
Daniel <23vietduc@gmail.com>
2022-10-01 21:51:11 +0200

    Testing CI

--- backend/src/db/models/index.js
+++ backend/src/db/models/index.js
@@ -6,11 +6,11 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const config = require('../../config');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 console.log(env);
-const config = require(__dirname + '/../config/config.js')[env];
 console.log(config);
 const db = {};
 
