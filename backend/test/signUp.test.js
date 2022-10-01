2db3482b16f1b81651a15532dcc94c6b2f33803a
Daniel <23vietduc@gmail.com>
2022-10-01 12:17:52 +0200

    Fixed test

--- backend/test/signUp.test.js
+++ backend/test/signUp.test.js
@@ -1,19 +1,10 @@
 import request from 'supertest';
 import app from '../src/app';
-import { migrations } from '../src/db/migrations';
-
-beforeAll(async () => {
-  await migrations.up();
-});
-
-afterAll(async () => {
-  await migrations.down();
-});
 
 describe('POST /api/register email tests : ', () => {
   it('responds with status code 400 given email is already taken', (done) => {
     const registerData = {
-      name: 'uniqueUser',
+      name: 'user',
       email: 'user1@example.com',
       password: 'Password.',
     };
@@ -22,8 +13,7 @@ describe('POST /api/register email tests : ', () => {
       .send(registerData)
       .expect(409)
       .end((err, res) => {
-        if (err) return done(err);
-        expect(res.body.message).toEqual('Email is already taken.');
+        if (err) return done(err).expect(res.body.message).toEqual('Email is already taken.');
         return done();
       });
   });
