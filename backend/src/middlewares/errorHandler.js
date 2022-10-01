dc5ca52fbb2cc85ef6dbc8886533e2b96658f054
Daniel <23vietduc@gmail.com>
2022-10-01 15:36:01 +0200

    Fix integration test

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -2,12 +2,13 @@ import logger from '../logger';
 import { errorMessages as errors } from '../errors/errorMessages';
 
 // eslint-disable-next-line no-unused-vars
-export default (err, req, res, next) => {
+export default (err, req, res) => {
   logger.error(
     `${errors[err.message].status || 500} - ${errors[err.message].message} - ${req.originalUrl} - 
     ${req.method} - ${req.ip}`,
   );
   return res
+    .set({ 'content-type': 'application/json; charset=utf-8' })
     .status(errors[err.message].status || 500)
     .json(errors[err.message].message || 'Internal server error');
 };
