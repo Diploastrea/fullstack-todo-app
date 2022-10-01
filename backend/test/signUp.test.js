73cc15db0df577b48a4ef3d955f0c52c999901a5
Daniel <23vietduc@gmail.com>
2022-10-01 17:25:46 +0200

    Fix integration test

--- backend/test/signUp.test.js
+++ backend/test/signUp.test.js
@@ -1,17 +1,38 @@
 import request from 'supertest';
 import app from '../src/app';
+import { User } from '../src/models/User';
 
-describe('POST /api/register email tests: ', () => {
-  it('responds with status code 409 given email is already taken', (done) => {
-    const registerData = {
-      name: 'user',
-      email: 'user1@example.com',
-      password: 'Password.',
-    };
+const mockDb = {
+  email: 'daniel@example.com',
+  password: '$2a$10$cMZtVjPNLmvrnnafdLOAo.0E8r6Cly3HNj5HTXFnYoOHtjPpmBwxe',
+};
+
+jest.mock('../src/models/User.js');
+
+beforeAll(() => jest.spyOn(User, 'findOne')
+  .mockReturnValue(Promise.resolve({
+    email: mockDb.email,
+    password: mockDb.password,
+  })));
+
+afterAll(() => jest.clearAllMocks());
+
+describe('POST /api/register email tests', () => {
+  it('responds with status code 200 given valid credentials', (done) => {
     request(app)
-      .post('/api/register')
-      .send(registerData)
+      .post('/api/login')
+      .send({
+        email: 'danifdas@example.com',
+        password: '1111',
+      })
       .set('Accept', 'application/json')
-      .expect(409, done);
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toHaveProperty('status', 'ok');
+        expect(res.body).toHaveProperty('token', expect.any(String));
+        return done();
+      });
   });
 });
