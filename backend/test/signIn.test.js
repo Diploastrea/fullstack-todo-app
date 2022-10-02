054a063c000d705fef1d130cb2c0d4bc41b0d57d
Daniel <23vietduc@gmail.com>
2022-10-02 21:43:27 +0200

    Added more signIn integration tests

--- backend/test/signIn.test.js
+++ backend/test/signIn.test.js
@@ -15,10 +15,35 @@ describe('POST /api/login with incorrect credentials', () => {
     const res = await request(app)
       .post('/api/login')
       .send({
-        email: 'email@example.com',
+        email: 'incorrectEmail@example.com',
         password: 'Password.',
       });
     expect(res.status).toEqual(401);
     expect(res.body.message).toEqual('Email or password is incorrect.');
+  });
+
+  it('responds with status code 401 given incorrect password', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'user1@example.com',
+        password: 'incorrectPassword',
+      });
+    expect(res.status).toEqual(401);
+    expect(res.body.message).toEqual('Email or password is incorrect.');
+  });
+});
+
+describe('POST /api/login with correct credentials', () => {
+  it('responds with status code 200 given correct credentials', async () => {
+    const res = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'user1@example.com',
+        password: 'Password.',
+      });
+    expect(res.status).toEqual(200);
+    expect(res.body.status).toEqual('ok');
+    expect(res.body.token).toEqual(expect.any(String));
   });
 });
