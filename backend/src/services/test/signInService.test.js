034e1c830ec7ac32123e0e69bf05c062b14fde80
Daniel <23vietduc@gmail.com>
2022-10-02 21:33:29 +0200

    Added more signInService unit tests

--- backend/src/services/test/signInService.test.js
+++ backend/src/services/test/signInService.test.js
@@ -1,4 +1,6 @@
-import { validateSignIn } from '../userService/signInService';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
+import { validateSignIn, generateToken } from '../userService/signInService';
 
 test('validateSignIn given no args', () => {
   const fnc = () => validateSignIn();
@@ -17,4 +19,22 @@ test('validateSignIn given no password', () => {
   const password = '';
   const fnc = () => validateSignIn(email, password);
   expect(fnc).toThrow('emptyPassword');
+});
+
+test('validateSignIn given valid args', () => {
+  const email = 'user@example.com';
+  const password = 'password';
+  const fnc = () => validateSignIn(email, password);
+  expect(fnc).not.toThrow(Error);
+});
+
+test('generateToken returns signed token', () => {
+  const user = {
+    id: 1,
+    name: 'Daniel',
+  };
+  const token = generateToken(user);
+  const decoded = jwt.verify(token, config.access_secret);
+  expect(decoded.id).toEqual(1);
+  expect(decoded.name).toEqual('Daniel');
 });
