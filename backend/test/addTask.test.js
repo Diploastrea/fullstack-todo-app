ecf9186878dde6bbbb9255022f1a1e05ee119b62
Daniel <23vietduc@gmail.com>
2022-10-03 23:51:17 +0200

    Added more adTask integration tests

--- backend/test/addTask.test.js
+++ backend/test/addTask.test.js
@@ -22,29 +22,29 @@ describe('POST /api/task', () => {
     expect(res.body.message).toEqual('Please enter a date in YYYY-MM-DD format.');
   });
 
-  // it('responds with status code 422 given due date in the past', async () => {
-  //   const res = await request(app)
-  //     .post('/api/task')
-  //     .set('Authorization', `Bearer ${token}`)
-  //     .send({
-  //       description: 'Do laundry',
-  //       priority: 'medium',
-  //       dueDate: '1999-01-01',
-  //     });
-  //   expect(res.status).toEqual(422);
-  //   expect(res.body.message).toEqual('Please enter a valid date!');
-  // });
+  it('responds with status code 422 given due date in the past', async () => {
+    const res = await request(app)
+      .post('/api/task')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        description: 'Do laundry',
+        priority: 'medium',
+        dueDate: '1999-01-01',
+      });
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual('Please enter a valid date!');
+  });
 
-  // it('responds with status code 201 and new task', async () => {
-  //   const res = await request(app)
-  //     .post('/api/task')
-  //     .set('Authorization', `Bearer ${token}`)
-  //     .send({
-  //       description: 'Do laundry',
-  //       priority: 'medium',
-  //       dueDate: '2025-01-01',
-  //     });
-  //   expect(res.status).toEqual(201);
-  //   expect(res.body.message).toEqual('Please enter a date in YYYY-MM-DD format.');
-  // });
+  it('responds with status code 201 and new task', async () => {
+    const res = await request(app)
+      .post('/api/task')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        description: 'Do laundry',
+        priority: 'medium',
+        dueDate: '2025-01-01',
+      });
+    expect(res.status).toEqual(201);
+    expect(res.body.message).toEqual('Please enter a date in YYYY-MM-DD format.');
+  });
 });
