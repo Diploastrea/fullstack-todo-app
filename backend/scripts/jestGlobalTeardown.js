7d4cbb1252df74b8b64f9f8d5f6b9496754b9f91
Daniel <23vietduc@gmail.com>
2022-10-04 00:54:00 +0200

    Fix jest global teardown

--- backend/scripts/jestGlobalTeardown.js
+++ backend/scripts/jestGlobalTeardown.js
@@ -3,6 +3,8 @@ import { migrations } from '../src/db/migrations';
 const { migrator, seeder } = migrations;
 
 export default async () => {
-  await seeder.down({ migrations: ['20220926201618-add-tasks.js', '20220922155118-add-users.js'] });
-  await migrator.down({ to: 0 });
+  await seeder.down();
+  await migrator.down();
+  await seeder.down();
+  await migrator.down();
 };
