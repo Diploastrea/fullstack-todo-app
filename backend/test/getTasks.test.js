58297137f96843ee3171540885fec97e046ef317
Daniel <23vietduc@gmail.com>
2022-10-05 22:18:59 +0200

    Added another getTasks integration test

--- backend/test/getTasks.test.js
+++ backend/test/getTasks.test.js
@@ -3,18 +3,30 @@ import app from '../src/app';
 import { generateToken } from '../src/services/userService/signInService';
 
 describe('GET /api/tasks', () => {
-  const user = {
+  const user1 = {
     id: 1,
     name: 'user',
   };
-  const token = generateToken(user);
+  const token1 = generateToken(user1);
+
+  const user2 = { ...user1, id: 2 };
+  const token2 = generateToken(user2);
 
   it('responds with status code 200 and user tasks', async () => {
     const res = await request(app)
       .get('/api/tasks')
-      .set('Authorization', `Bearer ${token}`);
+      .set('Authorization', `Bearer ${token1}`);
     expect(res.status).toEqual(200);
     expect(Array.isArray(res.body.tasks)).toEqual(true);
     expect(res.body.tasks.length).toEqual(2);
+  });
+
+  it('responds with status code 200 and no tasks', async () => {
+    const res = await request(app)
+      .get('/api/tasks')
+      .set('Authorization', `Bearer ${token2}`);
+    expect(res.status).toEqual(200);
+    expect(Array.isArray(res.body.tasks)).toEqual(true);
+    expect(res.body.tasks.length).toEqual(0);
   });
 });
