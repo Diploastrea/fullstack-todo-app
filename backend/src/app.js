d9340f57d2eea8e889a5c90af6bb405851578400
Daniel <23vietduc@gmail.com>
2022-10-09 22:06:11 +0200

    Updated api.routes.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import { api } from './routes';
 import logger from './logger';
@@ -6,6 +7,7 @@ import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('combined', { stream: logger.stream }));
 app.use('/api', api);
 app.use(errorHandler);
--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import { auth } from '../middlewares/auth';
 import {
   signUpController,
@@ -10,18 +9,7 @@ import {
 
 const router = express.Router();
 
-router.use(cors());
 router.use(express.json());
-
-router.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 router.post('/register', signUpController.signUpUser);
 
