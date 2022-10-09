7a6b4cbd5e2fb511291ffa011528c14930e324d1
Daniel <23vietduc@gmail.com>
2022-10-09 21:59:13 +0200

    Updated cors middleware

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -14,8 +14,12 @@ router.use(cors());
 router.use(express.json());
 
 router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
