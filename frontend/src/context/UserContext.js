27ed26113a4a7bd6487915e9c8099065ce44f65f
Daniel <23vietduc@gmail.com>
2022-10-23 22:05:29 +0200

    Added UserContext

---
+++ frontend/src/context/UserContext.js
@@ -0,0 +1,17 @@
+import jwtDecode from 'jwt-decode';
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext(null);
+
+export function UserProvider({ children }) {
+  const token = window.localStorage.getItem('token');
+  let decoded = null;
+  if (token) decoded = jwtDecode(token);
+  const user = useState(decoded ?? null);
+
+  return (
+    <UserContext.Provider value={user}>
+      {children}
+    </UserContext.Provider>
+  );
+}
